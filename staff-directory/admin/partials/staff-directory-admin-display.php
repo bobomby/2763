a55bb87d48d748a82777a98a208dd0c8eb8987bf
bobomby <bobbymokie@gmail.com>
2018-04-10 11:43:36 -0600

    Add files via upload

---
+++ staff-directory/admin/partials/staff-directory-admin-display.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       rmurphy42763.sb.cis
+ * @since      1.0.0
+ *
+ * @package    Staff_Directory
+ * @subpackage Staff_Directory/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<?php
+	global $post;
+	$custom = get_post_custom($post->ID);
+	$staff_directory_first_name = $custom["staff_directory_first_name"][0];
+	$staff_directory_last_name = $custom["staff_directory_last_name"][0];
+	$staff_directory_email = $custom["staff_directory_email"][0];
+	$staff_directory_sbio = $custom["staff_directory_sbio"][0];
+	$staff_directory_sort_order = $custom["staff_directory_sort_order"][0];
+	
+	
+		
+?>
+<fieldset class = "outer">
+<legend>Staff Directory Fields</legend>
+	<label class="staff-directory-label">First Name:</label>
+	<input class="staff-directory-input"  name="staff_directory_first_name" type="text" value="<?php echo $staff_directory_first_name; ?>"  required /><br>
+	<label class="staff-directory-label">Last Name:</label>
+	<input class="staff-directory-input"  name="staff_directory_last_name" type="text" value="<?php echo $staff_directory_last_name; ?>"  required /><br>
+	<label class="staff-directory-label">Email Address:</label>
+	<input class="staff-directory-input"  name="staff_directory_email" type="email" value="<?php echo $staff_directory_email; ?>"  required /><br>
+	<label class="staff-directory-label">Short Bio:</label>
+	<input class="staff-directory-input"  name="staff_directory_sbio" type="text" value="<?php echo $staff_directory_sbio; ?>"  required /><br>
+	<label class="staff-directory-label">Sort Order:</label>
+	<input class="staff-directory-input"  name="staff_directory_sort_order" type="text" value="<?php echo $staff_directory_sort_order; ?>"  required /><br>
+</fieldset>
